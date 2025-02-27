84cf6bdd866307ac950b58c5473b81fd20e74ceb
Jigsaw28 <levenko7@gmail.com>
2025-02-27 19:26:24 +0200

    update App.Styled

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,8 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container=styled.div`
-    padding: 40px;
-    height: 100vh;
-    text-align: center;
-    background-color: aliceblue;
-`
+export const Container = styled.div`
+  padding: 40px;
+  height: 100vh;
+  background-color: aliceblue;
+`;
+export const Title = styled.h2`
+  padding-bottom: 20px;
+`;
